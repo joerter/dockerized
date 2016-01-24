0f96c4f2762a8892a43c56c3ef344b5bb7854270
John Oerter <joerter@gmail.com>
2016-01-24 17:51:13 -0600

    Step 3

--- docker-run.sh
+++ docker-run.sh
@@ -1,3 +1,4 @@
 # !/bin/sh
 
-docker run --name dockerized-container -d -p 3000:3000 -v /Users/joerter/bench/dockerized/:/usr/src/app dockerized npm start 
+pwd=$(pwd)
+docker run --name dockerized-container -d -p 3000:3000 -v $pwd:/usr/src/app dockerized npm start 
