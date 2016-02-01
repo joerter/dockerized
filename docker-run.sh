f7601116bf536562f847e4d89f384f394eddaf1b
John Oerter <joerter@gmail.com>
2016-01-31 18:27:36 -0600

    Step 3

--- docker-run.sh
+++ docker-run.sh
@@ -1,4 +1,7 @@
 # !/bin/sh
 
 pwd=$(pwd)
-docker run --name dockerized-container -d -p 3000:3000 -v $pwd:/usr/src/app dockerized npm start 
+ip=$(docker-machine ip default)
+docker run --name dockerized-container -d -p 3000:3000 -v $pwd:/usr/src/app dockerized npm start && \
+echo "IP Address: $ip" && \
+docker attach --sig-proxy=false dockerized-container
