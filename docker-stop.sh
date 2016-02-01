57b7ecdd9060ef254040987131415b00f66e64c5
John Oerter <joerter@gmail.com>
2016-01-31 18:13:05 -0600

    Step 3

---
+++ docker-stop.sh
@@ -0,0 +1,3 @@
+# !/bin/sh
+
+docker stop dockerized-container && docker rm dockerized-container
--- server.js
+++ server.js
@@ -1,10 +1,27 @@
+'use strict';
+
 const http = require('http');
+const url = require('url');
 
 const port = 3000;
 
+const respond = (statusCode, msg, pathname, res) => {
+  res.writeHead(statusCode, {'content-type': 'text/plain'});
+  res.end(msg);
+  console.log(pathname, '-', statusCode, msg);
+}
+
 http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Dockerized Reloaded!\n');
+
+  const parsedRequest = url.parse(req.url, true);
+  if (parsedRequest.pathname !== '/login')
+    respond(404, 'Not Found', parsedRequest.path, res);
+
+  const queryString = parsedRequest.query;
+  if (queryString.username && queryString.password)
+    respond(200, 'Welcome, ' + queryString.username + '!', parsedRequest.path, res);
+  else
+    respond(403, 'Forbidden', parsedRequest.path, res);
 }).listen(port, () => {
   console.log(`Server running at http://0.0.0.0:${port}/`);
 });
